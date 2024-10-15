050bc9eaa9c4e1995588f54fc3c40bc34ab80140
24LP9083 24LP9083 <24LP9083@G301-CC0193.local>
2024-10-15 14:08:44 +0800

    register fe

--- frontend/src/app/(main)/register/page.tsx
+++ frontend/src/app/(main)/register/page.tsx
@@ -1,20 +1,32 @@
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
-const page = () => {
+const Register = () => {
     return (
         <div className="container flex justify-center m-auto">
-            <div className="w-full h-screen border border-black flex">
-                <div className="w-[722px] h-screen border border-black bg-red-500 flex justify-center items-center">
-                    <div className="w-[307px] h-[394px] border border-black">
-                        <h1 className="font-bold">HELLO!</h1>
-                        <h1>Sign Up to Get Started</h1>
-                        <Input className="w-[307px] h-[40px]  rounded-xl" />
+            <div className="  flex-1  h-screen border border-black bg-white flex justify-center items-center">
+                <div className="flex flex-col gap-10">
+                    <div>
+                        <h1 className=" text-xl font-bold text-[#0e343c]">HELLO!</h1>
+                        <h1 className="font-medium text-[#15444d]">Sign Up to Get Started</h1>
+                    </div>
+                    <div className="w-80 flex flex-col gap-5">
+                        <input type="text" className="border rounded-3xl py-3  px-4 border-[#15444d]" placeholder="Full Name " />
+                        <input type="text" className="border rounded-3xl py-3  px-4 border-[#15444d]" placeholder="Email Address " />
+                        <input type="text" className="border rounded-3xl py-3  px-4 border-[#15444d]" placeholder="Password " />
+                        <button className="border border-[#15444d] py-2 rounded-3xl hover:bg-[#226d7b] hover:text-white text-[#226d7b]">Register</button>
                     </div>
                 </div>
-                <div className="w-[722px] h-screen  border bg-[#226d78]"></div>
+            </div>
+            <div className="flex-1 h-screen  border bg-[#226d78] flex justify-center items-center">
+                <div className="w-[410px] h-[147px]  flex flex-col justify-between text-white">
+                    <h1 className="text-4xl font-bold">Hobby Merge</h1>
+                    <h1 className="text-xl ">The most popular peer to peer lending at SEA</h1>
+                    <h1>Read More</h1>
+                </div>
             </div>
         </div>
     )
 }
 
-export default page
+export default Register;
