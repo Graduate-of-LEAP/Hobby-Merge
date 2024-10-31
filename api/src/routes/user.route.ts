beaf7ad9f62c0d0f8511aeae407f042f12ecce9c
LPC243012 LPC243012 <LPC243012@G301-CC0201.local>
2024-10-31 12:40:17 +0800

    urgeljilne

--- api/src/routes/user.route.ts
+++ api/src/routes/user.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { SavedByCategoryId } from "../controllers/user/saved-by-categoryId.controller";
 import { getMe } from "../controllers/user/getme.controller";
-import { SavedByHobbyId } from "../controllers/user/saved-by-hobbyId.controller";
+import { addUserToHobby } from "../controllers/hobby/addUserstoHobby.controller";
 
 const userRouter = Router();
 
 userRouter
   .get("/", getMe)
   .post("/category", SavedByCategoryId)
-  .post("/hobby", SavedByHobbyId);
+  .post("/hobby", addUserToHobby);
 
 export { userRouter };
--- frontend/src/app/(main)/hobby/page.tsx
+++ frontend/src/app/(main)/hobby/page.tsx
@@ -1,19 +1,22 @@
 "use client";
+
 import {
   UserContextType,
   useUser,
-  Hobby,
+  Hobby as Hobbies,
 } from "@/components/utils/AuthProvider";
 import { api } from "@/lib/axios";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 
 const Hobby = () => {
   const { user, getUser } = (useUser() as UserContextType) || {};
+  const router = useRouter();
 
-  const [hobbies, setHobbies] = useState<Hobby[]>([]);
+  const [hobbies, setHobbies] = useState<Hobbies[]>([]);
   const [selectedHobbies, setSelectedHobbies] = useState<string[]>([]);
 
   const getHobbies = async () => {
@@ -39,25 +42,39 @@ const Hobby = () => {
     );
   };
 
+  const addUserToHobby = async (hobbyId: string, userId: string) => {
+    try {
+      const response = await api.post(
+        "/hobby/addUser",
+        { hobbyId, userId },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      console.log("User added to hobby:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error adding user to hobby:", error);
+      toast.error("Failed to add user to hobby.");
+      return { error };
+    }
+  };
+
   const saveSelectedHobbies = async () => {
     if (user) {
       console.log("User ID:", user._id);
       try {
-        const response = await api.post(
-          "/user/hobby",
-          {
-            userId: user._id,
-            hobbyIds: selectedHobbies,
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          }
+        const promises = selectedHobbies.map((hobbyId) =>
+          addUserToHobby(hobbyId, user._id)
         );
-        console.log(response.data);
+        await Promise.all(promises);
         getUser();
         toast.success("Таны hobby амжилттай хадгаллаа");
+        setSelectedHobbies([]);
+        router.push("/");
       } catch (error) {
         console.error("Error saving selected hobbies:", error);
         toast.error("Hobby хадгалахад алдаа гарлаа.");
@@ -71,6 +88,7 @@ const Hobby = () => {
     getHobbies();
     getUser();
   }, []);
+
   return (
     <div className="max-w-screen-xl m-auto h-screen border px-32">
       <div className="flex flex-col mt-[60px]">
@@ -79,7 +97,7 @@ const Hobby = () => {
         </div>
         <div className="mt-20 mb-10">
           <h1 className="text-[#06B6D4] font-semibold text-[30px] ">Hobby</h1>
-          <p className="text-[#6F6C90]"> Choose your hobbies</p>
+          <p className="text-[#6F6C90]">Choose your hobbies</p>
         </div>
         <div className="flex gap-2 flex-wrap h-fit ">
           {hobbies.map((hobby, index) => (
@@ -101,7 +119,7 @@ const Hobby = () => {
           <div className="mt-16">
             <button
               onClick={saveSelectedHobbies}
-              className="text-white bg-[942AE7] rounded-full w-64 p-2"
+              className="text-white bg-[#942AE7] rounded-full w-64 p-2"
             >
               Үргэлжлүүлэх
             </button>
